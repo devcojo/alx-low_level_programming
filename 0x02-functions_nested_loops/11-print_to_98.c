eefc82d913e790bae7ee256a0ccd2154ae507814
devcojo <dangab.alx23@gmail.com>
2023-06-21 10:25:27 -0700

    0-main.c, main.h

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,36 +1,38 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * print_to_98 - count the natural numbers from n to 98.
  * @n: value to count from.
- *
+ * return putchar() - function to print value of n.
+ *  putchar() - function to print value of n.
+ * @n: The number to begin counting at.
+ * times_table - times table
+ * times_table - prints the multiplication table from 1 to 9
+ * Return: void
  */
+
+int _putchar(char n)
+	{
+	return putchar(n);
+	}
+
+void times_table(void)
+{
 void print_to_98(int n)
 {
-	int i;
+	if (n >= 98)
+	{
+		while (n > 98)
+			printf("%d, ", n--);
+		printf("%d\n", n);
+	}
 
-	if (n == 98)
+	else
 	{
-		printf("98");
+		while (n < 98)
+			printf("%d, ", n++);
+		printf("%d\n", n);
 	}
-	else if (n < 98)
-	{
-		for (i = n ; i <= 98 ; i++)
-		{
-			printf("%d", i);
-			if (i < 98)
-				printf(", ");
-		}
-	}
-	else if (n > 98)
-	{
-		for (i = n ; i >= 98 ; i--)
-		{
-			printf("%d", i);
-			if (i > 98)
-				printf(", ");
-
-		}
-	}
-	printf("\n");
+}
 }
