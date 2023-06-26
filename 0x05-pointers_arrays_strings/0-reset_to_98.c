2c50982871540e790397916c0d7a31be3f927768
devcojo <dangab.alx23@gmail.com>
2023-06-26 04:43:04 -0700

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * update_to_98 - function to take a pointer to an int as parameter
+ * Rest_to_98 - function to take a pointer to an int as parameter
  * and update points to 98
  * @n: input
  * Return: n
