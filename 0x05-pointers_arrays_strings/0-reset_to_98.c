ff3453be3aaf423b2eef239e14b7e1300c134fe3
devcojo <dangab.alx23@gmail.com>
2023-06-26 04:39:39 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * update_to_98 - function to take a pointer to an int as parameter
+ * and update points to 98
+ * @n: input
+ * Return: n
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isdigit - check the given value is a digit
+ * @c: values to check
+ *
+ * Return: 1 if digit else 0 (False)
+ *
+ */
+
+int _isdigit(int c)
+{
+	if ()
+	{
+
+	}
+}
