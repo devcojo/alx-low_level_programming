43f78c9ee878dca3edc5325972669ade40997f80
devcojo <dangab.alx23@gmail.com>
2023-06-26 10:17:31 -0700

    7-puts_half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -3,29 +3,27 @@
   #include <stddef.h>
 
 /**
- * puts_half - Prints the second half of a string
+ * puts_half - Prints the second half of a string, followed by a new line
  * @str: Pointer to the string
  *
  * Description: This function takes a pointer to a null-terminated string
- * and prints the second half of the string, followed by a new line.
- * If the number of characters is odd, it prints the last n characters
- * of the string, where n = (length_of_the_string - 1) / 2.
+ * and prints the second half of the string, followed by a new line. If the
+ * number of characters is odd, it prints the last n characters of the string,
+ * where n = (length_of_the_string - 1) / 2.
  */
 
 void puts_half(char *str)
 {
 	int length = 0;
-	int i, start_index;
+	int start_index;
+	int i;
 
 	/* Calculate the length of the string */
 	while (str[length] != '\0')
 		length++;
 
-	/* Calculate the start index for printing */
-	if (length % 2 == 0)
-		start_index = length / 2;
-	else
-		start_index = (length - 1) / 2;
+	/* Calculate the starting index based on string length */
+	start_index = (length - 1) / 2 + 1;
 
 	/* Print the second half of the string */
 	for (i = start_index; i < length; i++)
@@ -34,4 +32,3 @@ void puts_half(char *str)
 	/* Print a new line */
 	putchar('\n');
 }
-
