15ea51df692269210b68225ca1ab7b509e960a35
devcojo <dangab.alx23@gmail.com>
2023-07-05 11:22:56 -0700

    0-puts_recursion.c

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,13 @@
+/**
+ * Desc: Header file containing prototypes for all functions
+ *       used in the 0x07-recursion directory.
+ */
+
+int _putchar(char c);
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int is_prime_number(int n);
